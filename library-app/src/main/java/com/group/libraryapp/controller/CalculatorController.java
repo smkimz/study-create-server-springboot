e6262aa8c6a2bf568e30c436e76a5606c526b0d7
unknown <smkim5698@gmail.com>
2024-02-22 00:02:49 +0900

    section_01

---
+++ library-app/src/main/java/com/group/libraryapp/controller/CalculatorController.java
@@ -0,0 +1,23 @@
+package com.group.libraryapp.controller;
+
+import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CalculatorController {
+    @GetMapping("/add")
+    public int addTwoNumbers(CalculatorAddRequest request) {
+        return request.getNum1() + request.getNum2();
+    }
+
+    @PostMapping("/multiply") // Post /multiply
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNum1() * request.getNum2();
+    }
+
+}
